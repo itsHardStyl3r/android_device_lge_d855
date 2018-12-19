e88db0b7c8660d26f933b80e1aaa4c38f2b397e3
Aleksander Gencel <johnnylittleplanet@gmail.com>
2018-12-19 21:47:56 +0100

    AOSPExtended for d855

--- aosp_d855.mk
+++ aosp_d855.mk
@@ -1,9 +1,10 @@
 $(call inherit-product, device/lge/d855/full_d855.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSPExtended stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1440
 
-PRODUCT_NAME := lineage_d855
+PRODUCT_NAME := aosp_d855
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
