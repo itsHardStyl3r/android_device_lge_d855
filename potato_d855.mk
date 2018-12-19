3a133b48ca46f01095816c921efeaa7d16240af1
Aleksander Gencel <johnnylittleplanet@gmail.com>
2018-12-19 22:52:08 +0100

    PotatoProject for d855

--- potato_d855.mk
+++ potato_d855.mk
@@ -20,11 +20,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from d855 device
 $(call inherit-product, device/lge/d855/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common POSP stuff.
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="hardStyl3r"
 
 PRODUCT_DEVICE := d855
-PRODUCT_NAME := lineage_d855
+PRODUCT_NAME := potato_d855
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-D855
 PRODUCT_MANUFACTURER := LGE
