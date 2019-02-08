3d0b8545ce70993aa8e6cf137b47ca3edf183271
Aleksander Gencel <johnnylittleplanet@gmail.com>
2019-02-09 00:22:09 +0100

    ResurrectionRemix for d855

--- rr_d855.mk
+++ rr_d855.mk
@@ -20,11 +20,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from d855 device
 $(call inherit-product, device/lge/d855/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ResurrectionRemix stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := d855
-PRODUCT_NAME := lineage_d855
+PRODUCT_NAME := rr_d855
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-D855
 PRODUCT_MANUFACTURER := LGE
